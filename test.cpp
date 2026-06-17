72ba513f76053a5e993e47422342b3a05e5ae66d
AhmadSaeedZaidi <ahmadsaeedzaidi@gmail.com>
2026-06-17 13:06:25 +0500

    feat: product function

--- test.cpp
+++ test.cpp
@@ -12,10 +12,19 @@ int add(int (&value)[N]) {
     return sum;
 }
 
+int product(int (&value)[N]){
+    int prod = 1;
+    for (auto v : value) {
+        prod*=v;
+    }
+    return prod;
+}
+
 // small comment here to add a change in the commit history
 
 int main() {
     int value[] = {1,2,3,4};
     std::print("sum: {}\n",add(value));
+    std::print("prod {}\n", prod(value))
     std::print("another small change\n");
 }
