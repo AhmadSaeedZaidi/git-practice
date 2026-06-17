6dcff4f50e430c1a85e7d7edb4d593132f21ce46
AhmadSaeedZaidi <ahmadsaeedzaidi@gmail.com>
2026-06-17 13:06:25 +0500

    feat: product function

--- test.cpp
+++ test.cpp
@@ -11,6 +11,14 @@ int add(int (&value)[N]) {
     return sum;
 }
 
+int product(int (&value)[N]){
+    int prod = 1;
+    for (auto v : value) {
+        prod*=v;
+    }
+    return prod;
+}
+
 // small comment here to add a change in the commit history
 template <size_t N>
 int max(int(&value)[N]) {
@@ -24,5 +32,6 @@ int max(int(&value)[N]) {
 int main() {
     int value[] = {1,2,3,4};
     std::print("sum: {}\n",add(value));
+    std::print("prod {}\n", prod(value))
     std::print("another small change\n");
 }
